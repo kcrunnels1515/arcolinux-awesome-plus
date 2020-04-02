e29c5948fc0da5b3df448b57318a19c64cfbf127
Kelly Runnels <kcrunnels15@gmail.com>
2020-04-02 15:01:19 -0400

    Dis be de first, bitch.

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+WHOM=$(whoami)
+HERE=$(pwd)
+reqpkgs=(awesome vicious conky awesome-freedesktop nitrogen rofi manjaro-zsh-config)
+installpkgs() {
+  sudo pacman -S ${reqpkgs[@]}
+  cpbins
+}
+
+cpobjs() {
+  cp -r $HERE/awesome /home/$WHOM/.config/
+  sudo cp $HERE/st /usr/local/bin/
+  cat zshrc > /home/$WHOM/.zshrc
+}
+
+set-wallpp() {
+  CATFISH=$(find ~ -type f -name "catfish*")
+  nitrogen --set-scaled $CATFISH
+}
+function yes_or_no {
+    while true; do
+      read -p "$* [y/n]: " yn
+      case $yn in
+        [Yy]*) installpkgs ;;  
+        [Nn]*) echo "Aborted" ; return  1 ;;
+      esac
+    done
+  }
+yes_or_no
